18da50d89db0d47b69d67272f7a01f7e074899f5
Kacper Sokol <ks1591@my.bristol.ac.uk>
2017-06-14 15:50:10 +0200

    Standalone plots.

---
+++ docs/standalone/Readme.md
@@ -0,0 +1,29 @@
+# Standalone plots for JGI Pure Challenge #
+This folder contains 5 plots.
+
+## University of Bristol structure ##
+**[`tree_uni.html`](https://so-cool.github.io/UoBrain/standalone/tree_uni.html)**
+
+This plot shows the tree structure of the University of Bristol in a form of a *raidal tree*.
+
+## University of Bristol academic unit structure ##
+**[`tree_acad.html`](https://so-cool.github.io/UoBrain/standalone/tree_acad.html)**
+
+This plot shows the tree structure of the academic units at the University of Bristol in a form of a *raidal tree*.
+
+## Faculties interconnections ##
+**[`chord_fac.html`](https://so-cool.github.io/UoBrain/standalone/chord_fac.html)**
+
+This plot shows the strength of connections between the faculties at the University of Bristol based on the number of scientific publications that each faculty collaborated on in every given year and in total.  
+The connection strength over the whole period 2008--2013 is a weighted average of yearly connections where the weight is more significant for the more recent years.
+
+## Schools interconnections ##
+**[`chord_sch.html`](https://so-cool.github.io/UoBrain/standalone/chord_sch.html)**
+
+This plot shows the strength of connections between the academic schools at the University of Bristol based on the number of scientific publications that each school collaborated on in every given year and in total.  
+The connection strength over the whole period 2008--2013 is a weighted average of yearly connections where the weight is more significant for the more recent years.
+
+## Possible school collaborations ##
+**[`chord_col.html`](https://so-cool.github.io/UoBrain/standalone/chord_col.html)**
+
+This plot shows possible connections between academic schools at the University of Bristol based on similarity (cosine distance) between tokenised publication titles produced by each academic school.
---
+++ docs/standalone/tree_uni.js
@@ -0,0 +1,180 @@
+var vegaJSONSpec = {
+  "$schema": "https://vega.github.io/schema/vega/v3.0.json",
+  "width": 700,
+  "height": 700,
+  "padding": 5,
+  "autosize": "none",
+
+  "signals": [
+    {
+      "name": "labels", "value": true,
+      "bind": {"input": "checkbox"}
+    },
+    {
+      "name": "layout", "value": "tidy",
+      "bind": {"input": "radio", "options": ["tidy", "cluster"]}
+    },
+    {
+      "name": "radius", "value": 320,
+      "bind": {"input": "range", "min": 20, "max": 600}
+    },
+    { "name": "originX", "update": "width / 2" },
+    { "name": "originY", "update": "height / 2" },
+    {
+      "name": "tooltip",
+      "value": {},
+      "on": [
+        {"events": "symbol:mouseover", "update": "datum"},
+        {"events": "symbol:mouseout",  "update": "{}"}
+      ]
+    }
+  ],
+
+  "data": [
+    {
+      "name": "tree",
+      "url": "../data/uni_tree.json",
+      "transform": [
+        {
+          "type": "stratify",
+          "key": "id",
+          "parentKey": "parent"
+        },
+        {
+          "type": "tree",
+          "method": {"signal": "layout"},
+          "size": [1, {"signal": "radius"}],
+          "as": ["alpha", "radius", "depth", "children"]
+        },
+        {
+          "type": "formula",
+          "expr": "(240 + 360 * datum.alpha + 270) % 360",
+          "as":   "angle"
+        },
+        {
+          "type": "formula",
+          "expr": "PI * datum.angle / 180",
+          "as":   "radians"
+        },
+        {
+          "type": "formula",
+          "expr": "inrange(datum.angle, [90, 270])",
+          "as":   "leftside"
+        },
+        {
+          "type": "formula",
+          "expr": "originX + datum.radius * cos(datum.radians)",
+          "as":   "x"
+        },
+        {
+          "type": "formula",
+          "expr": "originY + datum.radius * sin(datum.radians)",
+          "as":   "y"
+        }
+      ]
+    },
+    {
+      "name": "links",
+      "source": "tree",
+      "transform": [
+        { "type": "treelinks", "key": "id" },
+        {
+          "type": "linkpath",
+          "shape": "diagonal", "orient": "radial",
+          "sourceX": "source.radians", "sourceY": "source.radius",
+          "targetX": "target.radians", "targetY": "target.radius"
+        }
+      ]
+    }
+  ],
+
+  "scales": [
+    {
+      "name": "color",
+      "type": "sequential",
+      "range": {"scheme": "magma"},
+      "domain": {"data": "tree", "field": "depth"},
+      "zero": true
+    }
+  ],
+
+  "marks": [
+    {
+      "type": "path",
+      "from": {"data": "links"},
+      "encode": {
+        "update": {
+          "x": {"signal": "originX"},
+          "y": {"signal": "originY"},
+          "path": {"field": "path"},
+          "stroke": {"value": "#ccc"}
+        }
+      }
+    },
+    {
+      "type": "symbol",
+      "from": {"data": "tree"},
+      "encode": {
+        "enter": {
+          "size": {"value": 100},
+          "stroke": {"value": "#fff"}
+        },
+        "update": {
+          "x": {"field": "x"},
+          "y": {"field": "y"},
+          "fill": {"scale": "color", "field": "depth"}
+        },
+        "hover": {
+          "fill": {"value": "red"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {"data": "tree"},
+      "encode": {
+        "enter": {
+          "text": {"field": "name"},
+          "fontSize": {"value": 9},
+          "baseline": {"value": "middle"}
+        },
+        "update": {
+          "x": {"field": "x"},
+          "y": {"field": "y"},
+          "dx": {"signal": "(datum.leftside ? -1 : 1) * 6"},
+          "angle": {"signal": "datum.leftside ? datum.angle - 180 : datum.angle"},
+          "align": {"signal": "datum.leftside ? 'right' : 'left'"},
+          "opacity": {"signal": "labels ? 1 : 0"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "encode": {
+        "enter": {
+          "baseline": {"value": "bottom"},
+//                "fill": {"value": "red"},
+//                "stroke": {"value": "black"}
+          "font": {"value": "monospace"},
+          "fontSize": {"value": 17},
+        },
+        "update": {
+          "align": {"value": "left"},
+          "x": {"value": 10},
+          "y": {"value": 10},
+          "text": {"signal": "tooltip.description"},
+          "fillOpacity": [
+            {"test": "datum === tooltip", "value": 0},
+            {"value": 1}
+          ]
+        }
+      }
+    }
+  ]
+};
+
+var view = new vega.View(vega.parse(vegaJSONSpec))
+  .renderer('svg')          // set renderer (canvas or svg)
+  .initialize('#university') // initialize view within parent DOM container
+  .hover()                     // enable hover encode set processing
+  .run();                      // run the dataflow and render the view
